fca9d8c6b63f0f49ecdd083cd73a4f1cc7d49d7a
HipsterBrown <headhipster@hipsterbrown.com>
2022-02-05 16:36:30 -0500

    feat: create http-service module, fix rgb-led methods

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+import Timer from "timer";
 import Net from "net";
-import { Server } from "http";
 import MDNS from "mdns";
-import PWM from "pins/pwm";
-import { RGB } from "./lib/rgb-led";
+import { RGB } from "rgb-led";
+import { HTTPService } from "http-service";
+
+const led = new RGB([12, 13, 14]);
 
 let hostName = "on-air";
 
@@ -10,52 +12,62 @@ trace("Setup complete!\n");
 trace(Net.get("SSID") + "\n");
 trace(Net.get("IP") + "\n");
 
-new MDNS({ hostName }, function (message, value) {
-  if (message === MDNS.hostName) {
-    if (value) {
-      hostName = value;
-      this.add({ name: "onair", protocol: "tcp", port: 80, txt: {} });
-      trace(`${hostName} hostname claimed!\n`);
-    } else {
-      trace(`Working on mdns claim\n`);
+const serviceReady = new Promise((resolve) => {
+  new MDNS({ hostName }, function (message, value) {
+    if (message === MDNS.hostName) {
+      if (value) {
+        hostName = value;
+        this.add({ name: "onair", protocol: "tcp", port: 80, txt: {} });
+        trace(`${hostName} hostname claimed!\n`);
+        resolve();
+      } else {
+        trace(`Working on mdns claim\n`);
+      }
     }
+  });
+});
+
+serviceReady.then(() => {
+  led.green();
+  Timer.delay(3000);
+  led.off();
+});
+
+const service = new HTTPService();
+
+service.onError(() => {
+  throw new Error("ServerError");
+});
+
+service.get("/api/state", () => {
+  return {
+    body: String(led.currentColor),
+  };
+});
+
+service.post("/api/toggle", () => {
+  led.toggle();
+});
+
+service.post("/api/color", (request) => {
+  try {
+    const { color } = JSON.parse(request.body);
+    if (led[color] !== undefined) {
+      led[color]();
+    } else if (Array.isArray(color)) {
+      led.setColor(color);
+    } else {
+      throw new Error("Invalid color value");
+    }
+    return { status: 204 };
+  } catch (error) {
+    trace(`Unable to parse ${request.body}\n`);
+    return { status: 422, body: error.toString() };
   }
 });
 
-const led = new RGB([12, 13, 14]);
-led.red();
-
-const server = new Server({});
-
-server.callback = function (message, val1, val2) {
-  switch (message) {
-    case Server.connection:
-      trace(`New server connection: ${val1}\n`);
-      break;
-    case Server.status:
-      this.path = val1;
-      trace(`Request status: ${val1} + ${val2}\n`);
-      break;
-    case Server.header:
-      trace(`Request header: ${val1} : ${val2}\n`);
-    case Server.headersComplete:
-      return String;
-    case Server.requestComplete:
-      trace(`Request complete! ${val1} ${val2}\n`);
-    case Server.prepareResponse:
-      trace(`Responding to request\n`);
-      if (this.path.includes("state")) {
-        led.blue();
-      }
-      return {
-        status: 200,
-        headers: ["Content-type", "text/plain"],
-        body: "OK",
-      };
-  }
-};
-
-// TODO:
-// - cleanup HTTP service abstraction, i.e. AdvertisedServer, to include MDNS setup, easier route handlers
-// - figure out status colors: (green === service ready, blue === active, none === inactive, red === error)
-// - profit ?
+/**
+TODO:
+- basic web UI
+- more refinement of color control
+**/
